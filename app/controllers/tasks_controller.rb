1f04f26b58a0b379ca7d521b339ce60d0ade6d88
kentaro.ashizawa <ken.aszw@gmail.com>
2018-06-24 02:21:38 +0000

    課題タスク

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,7 @@ class TasksController < ApplicationController
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
+    end
   end
 
   def edit
@@ -27,11 +28,25 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
-end
+  
     private
     
   # Strong Parameter
